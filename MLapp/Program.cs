9aa499ad3dcce46ec9f3e688395f295580fff0f8
janus.o@live.dk <janus.o@live.dk>
2022-08-12 18:53:51 +0200

    Add project files.

---
+++ MLapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using MLapp;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Load sample data
+var imageBytes = File.ReadAllBytes(@"D:\Downloads\archive (4)\pokemon\Abra\00000000.png");
+MLModel1.ModelInput sampleData = new MLModel1.ModelInput()
+{
+    ImageSource = imageBytes,
+};
+
+//Load model and predict output
+var result = MLModel1.Predict(sampleData);
+Console.WriteLine(result.PredictedLabel);
